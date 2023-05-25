4597f70dfd136e9c55d367ec376e8f27e3fff2bc
OksanaPod31 <aksanapodkovyrova@yandex.ru>
2023-05-25 17:20:00 +0300

    AuthenticationState

--- BlockchainApp.Web/Client/IdentityAuthenticationStateProvider.cs
+++ BlockchainApp.Web/Client/IdentityAuthenticationStateProvider.cs
@@ -1,21 +1,35 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
 
 namespace BlockchainApp.Web.Client
 {
     public class IdentityAuthenticationStateProvider : AuthenticationStateProvider
     {
+        private readonly ILocalStorageService localStorageService;
+
+        public IdentityAuthenticationStateProvider(ILocalStorageService localStorageService)
+        {
+            this.localStorageService = localStorageService;
+        }
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            
-            return new AuthenticationState(MarkUserAsAuthenticated("jhj"));
-        }
-        public ClaimsPrincipal MarkUserAsAuthenticated(string token)
-        {
-            if(string.IsNullOrEmpty(token)) { return new ClaimsPrincipal(); }
-            return new ClaimsPrincipal(new ClaimsIdentity(
+            var token = await localStorageService.GetItemAsStringAsync("token");
+            if(!string.IsNullOrEmpty(token))
+            {
+                var authUser = new ClaimsPrincipal(new ClaimsIdentity(
                 new List<Claim>() { new Claim(ClaimTypes.Name, "admin") }, "jwt"));
+                return new AuthenticationState(authUser);
+            }
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+        public void MarkUserAsAuthenticated(string token)
+        {
 
+            var authUser = new ClaimsPrincipal(new ClaimsIdentity(
+                new List<Claim>() { new Claim(ClaimTypes.Name, "admin") }, "jwt"));
+            var authState = Task.FromResult(new AuthenticationState(authUser));
+            NotifyAuthenticationStateChanged(authState);
 
         }
     }
